0cacfff6e559b40a8c626c6653c6c41d28129b0b
sj0delacruz <sj.delacruz@uniandes.edu.co>
2019-05-23 16:52:56 +0000

    correcion

---
+++ delaCruzSamir_final_16.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("coordenada x: 40.2 +/- 10.3\n")
+print("coordenada y: 40.2 +/- 10.3\n")
+print("tiempo de lanzamiento: 40.2 +/- 10.3\n")
+print("velocidad de lanzamiento: 40.2 +/- 10.3\n")
